f93f23f5ee255cca9fe7e3822345c5742f28c5fd
mayankdev <mayank.chaturvedi@infoobjects.com>
2016-04-15 11:59:34 +0530

    Insert login bug resolved

--- HRMS.UI/app/factory/authFctry.js
+++ HRMS.UI/app/factory/authFctry.js
@@ -1,26 +1,69 @@
 hrBaseApp.factory('authFctry', ['$http', '$resource', '$q', function ($http, $resource, $q) {
     var authDataFctry = {};
-  //  var resource = $resource("http://mayank-pc:9095" + '/account/:action', {
+    //  var resource = $resource("http://mayank-pc:9095" + '/account/:action', {
     var resource = $resource("http://localhost:9095" + '/account/:action', {
         action: '@action',
-    },
-    {
-        'login': { method: 'POST', params: { action: 'login' }, isArray: false }
-    }
-    );
+    }, {
+        'login': {
+            method: 'POST',
+            params: {
+                action: 'login'
+            },
+            isArray: false
+        },
+        'insertLoginData': {
+            method: 'POST',
+            params: {
+                action: 'insertLoginData'
+            },
+            isArray: false
+        },
+        'insertEmpDetails': {
+            method: 'POST',
+            params: {
+                action: 'insertEmpDetails'
+            },
+            isArray: false
+        }
+    });
     var _login = function (parms) {
         var deferred = $q.defer();
         resource.login(parms,
             function (response) {
                 deferred.resolve(response);
             },
-          function (response) {
-              deferred.reject(response);
-          });
+            function (response) {
+                deferred.reject(response);
+            });
 
         return deferred.promise;
     }
+    var _insertLoginData = function (parms) {
+        var deferred = $q.defer();
+        resource.insertLoginData(parms,
+            function (response) {
+                deferred.resolve(response);
+            },
+            function (response) {
+                deferred.reject(response);
+            });
 
+        return deferred.promise;
+    }
+    var _insertEmpDetails = function (parms) {
+        var deferred = $q.defer();
+        resource.insertEmpDetails(parms,
+            function (response) {
+                deferred.resolve(response);
+            },
+            function (response) {
+                deferred.reject(response);
+            });
+
+        return deferred.promise;
+    }
+    authDataFctry.insertLoginData = _insertLoginData;
+    authDataFctry.insertEmpDetails = _insertEmpDetails;
     authDataFctry.login = _login;
     return authDataFctry;
 }]);
