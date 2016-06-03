36cd0a4bd2290455d6aca1f69a2909dc0172429b
mahendraagrawal705 <mahendra@infoobjects.com>
2016-06-03 15:48:57 +0530

    code change for sending mail

--- HRMS.Api/HRMS.Api/routes/feedback.js
+++ HRMS.Api/HRMS.Api/routes/feedback.js
@@ -19,57 +19,54 @@ var config = {
 };
 
 router.post('/submitFeedback', function (req, res) {
-   var connection = new sqlConnection(config);
-    connection.on('connect', function (err) {
-        if (err) {
-            return console.error(err);
-        }
-        // If no error, then good to proceed.
-        executeStatement();
-    });
-    var Request = require('tedious').Request;
-    var TYPES = require('tedious').TYPES;
+//    var connection = new sqlConnection(config);
+//     connection.on('connect', function (err) {
+//         if (err) {
+//             return console.error(err);
+//         }
+//         // If no error, then good to proceed.
+//         executeStatement();
+//     });
+//     var Request = require('tedious').Request;
+//     var TYPES = require('tedious').TYPES;
 
-    function executeStatement() {
-        request = new Request("exec sp_InsertAnonymousFeedback @FeedbackId, @Subject, @Description, @Resolve, @FeedbackDateTime ", function (err, rowCount, rows) {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('sp_InsertAnonymousFeedback');
-                console.log(rows);
-                if (rowCount > 0) {
-                    res.json({
-                        type: true,
-                        data: rows
-                    });
-                }
-            }
-        });
-        request.addParameter('FeedbackId', TYPES.VarChar, req.body.FeedbackId);
-        request.addParameter('Subject', TYPES.VarChar, req.body.Feedback.Subject);
-        request.addParameter('Description', TYPES.VarChar, req.body.Description);
-        request.addParameter('Resolve', TYPES.VarChar, 0);
-        request.addParameter('FeedbackDateTime', TYPES.VarChar, NOW());
-        connection.execSql(request);
-    }
+//     function executeStatement() {
+//         request = new Request("exec sp_InsertAnonymousFeedback @FeedbackId, @Subject, @Description, @Resolve, @FeedbackDateTime ", function (err, rowCount, rows) {
+//             if (err) {
+//                 console.log(err);
+//             } else {
+//                 console.log('sp_InsertAnonymousFeedback');
+//                 console.log(rows);
+//                 if (rowCount > 0) {
+//                     res.json({
+//                         type: true,
+//                         data: rows
+//                     });
+//                 }
+//             }
+//         });
+//         request.addParameter('FeedbackId', TYPES.VarChar, req.body.feedback.FeedbackId);
+//         request.addParameter('Subject', TYPES.VarChar, req.body.feedback.Subject);
+//         request.addParameter('Description', TYPES.VarChar, req.body.feedback.Description);
+//         request.addParameter('Resolve', TYPES.VarChar, 0);
+//         request.addParameter('FeedbackDateTime', TYPES.VarChar, NOW());
+//         connection.execSql(request);
+//     }
    
     // Not the movie transporter!
     var transporter = nodemailer.createTransport({
         service: 'Gmail',
         auth: {
-            user: 'mahendra@infoobjects.com', // Your email id
-            pass: 'm@nish705' // Your password
+            user: 'hrms.infoobjects@gmail.com', // Your email id
+            pass: 'hrms@infoobjects' // Your password
         }
     });
     var mailOptions={
-        from:'example@gmail.com',
-             to:req.body.Feedback.Email,
-                  subject:req.body.Feedback.Subject,
-                   text:req.body.Feedback.Description
+             to:req.body.feedback.email,
+                  subject:req.body.feedback.subject,
+                   text:req.body.feedback.description
     }
 sendEmail(mailOptions);
-});
-
  function sendEmail(mailOptions) {
      transporter.sendMail(mailOptions, function(error, info){
     if(error){
@@ -81,4 +78,7 @@ sendEmail(mailOptions);
     };
 });
  }
+});
+
+
 module.exports = router;
