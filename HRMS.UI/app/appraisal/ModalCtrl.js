9c7612b7713410b35e049fd08b786e09d477e8a3
damini-jain <damini@infoobjects.com>
2016-07-21 13:02:35 +0530

    Appraisal module modified
    Appraisal module modified

--- HRMS.UI/app/appraisal/ModalCtrl.js
+++ HRMS.UI/app/appraisal/ModalCtrl.js
@@ -1,31 +1,14 @@
 
 'use strict';
-hrBaseApp.controller('hrmsAppraisalCtrl', ['$scope', '$modal', function ($scope, $modal) {
-    $scope.Submit = function (size) {
-        console.log("Review Pending");
-          
-        $scope.opts = {
-            backdrop: true,
-            backdropClick: true,
-            dialogFade: false,
-            keyboard: true,
-            templateUrl: '/app/appraisal/Modal.html',
-            controller: ModalInstanceCtrl,
-            size:size,
-            resolve: {} // empty storage
-        };
+hrBaseApp.controller('ModalCtrl', ['$scope', '$modal','$modalInstance', function ($scope, $modal, $modalInstance) {
+    $scope.names = ["1", "2", "3", "4", "5", "6", "7","8","9","10"];
+    $scope.questions=["ffdgsdgsdg","fdvghfvghds","fsfsfsfsdfsdgf","fsfffvgdfvdf"];
+    $scope.ok = function () {
+        $modalInstance.close();
+    };
 
-        $scope.opts.resolve.item = function () {
-            return angular.copy({ name: "Damini" }); // pass name to Dialog
-        }
-
-        var modalInstance = $modal.open($scope.opts);
-
-        modalInstance.result.then(function () {
-            //on ok button press 
-        }, function () {
-            //on cancel button press
-            console.log("Modal Closed");
-        });
+    $scope.cancel = function () {
+        $modalInstance.dismiss('cancel');
     };
 }]);
+
--- HRMS.UI/app/appraisal/hrmsAppraisalCtrl.js
+++ HRMS.UI/app/appraisal/hrmsAppraisalCtrl.js
@@ -1,44 +1,36 @@
 
-'use strict';
-hrBaseApp.controller('hrmsAppraisalCtrl', ['$scope', '$modal', function ($scope, $modal) {
-    $scope.click = function (size) {
-        console.log("Review Pending");
-          
-        $scope.opts = {
-            backdrop: true,
-            backdropClick: true,
-            dialogFade: false,
-            keyboard: true,
-            templateUrl: '/app/appraisal/Modal.html',
-            controller: ModalInstanceCtrl,
-            size:size,
-            resolve: {} // empty storage
-        };
+hrBaseApp.controller('hrmsAppraisalCtrl', ['$scope', '$modal',
+    function ($scope, $modal) {
+        'use strict';
 
-        $scope.opts.resolve.item = function () {
-            return angular.copy({ name: "Damini" }); // pass name to Dialog
-        }
+            $scope.click = function () {
+                $scope.opts = {
+                    backdrop: true,
+                    backdropClick: true,
+                    dialogFade: false,
+                    keyboard: true,
+                    templateUrl: '/app/appraisal/Modal.html',
+                    controller: 'ModalCtrl',
+                    size: 'lg',
+                    resolve: {} // empty storage
+                };
 
-        var modalInstance = $modal.open($scope.opts);
+                $scope.opts.resolve.item = function () {
+                    return angular.copy({ name: "Damini" }); // pass name to Dialog
+                }
 
-        modalInstance.result.then(function () {
-            //on ok button press 
-        }, function () {
-            //on cancel button press
-            console.log("Modal Closed");
-        });
-    };
-}]);
-var ModalInstanceCtrl = function ($scope, $modalInstance, $modal, item) {
+                var modalInstance = $modal.open($scope.opts);
 
-    $scope.item = item;
-    $scope.names = ["1", "2", "3", "4", "5", "6", "7","8","9","10"];
-    $scope.questions=["ffdgsdgsdg","fdvghfvghds","fsfsfsfsdfsdgf","fsfffvgdfvdf"];
-    $scope.ok = function () {
-        $modalInstance.close();
-    };
+                modalInstance.result.then(function () {
+                    //on ok button press 
+                }, function () {
+                    //on cancel button press
+                    console.log("Modal Closed");
+                });
+            };
+    }]);
 
-    $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-    };
-}
+    
+
+
+
